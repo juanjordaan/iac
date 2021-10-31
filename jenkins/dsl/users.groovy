b6d166b9cc9d75d6c766e31d6b5bbb4bd712d39d
Juan Jordaan <juan.jordaan1234@gmail.com>
2021-10-31 16:32:47 +0200

    Initial commit

---
+++ jenkins/dsl/users.groovy
@@ -0,0 +1,26 @@
+def String projectDir = 'projects/next-location'
+
+job('Users API Jenkins DSL') {
+	jdk('openjdk-11')
+
+	scm {
+		git {
+			remote {
+				url('https://github.com/juanjordaan/users-api')
+			}
+			branch('*/failing_tests')
+		}
+	}
+
+	triggers {
+		scm('* * * * *')
+	}
+
+	steps {
+		maven {
+			mavenInstallation('maven_3_8_3')
+			rootPOM("${projectDir}/pom.xml")
+			goals('test')
+		}
+	}
+}
